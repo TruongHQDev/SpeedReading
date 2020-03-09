97141f4d274d858a375b9f0292565c7126c555a5
AUTO SERVER <vt001@ASs-Mac-mini.local>
2020-03-09 16:02:02 +0700

    Add folder

---
+++ SpeedReading/SpeedReading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedReading
+//
+//  Created by AUTO SERVER on 3/9/20.
+//  Copyright © 2020 TruongHa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
